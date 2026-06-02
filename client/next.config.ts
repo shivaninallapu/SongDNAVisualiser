881495e424fa1b6262027b3e632a7fca01d1bcb1
Shivani Nallapu <shivaninallapu@gmail.com>
2026-06-02 18:38:47 -0400

    fix: production next config

--- client/next.config.ts
+++ client/next.config.ts
@@ -5,7 +5,9 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://127.0.0.1:8000/:path*",
+        destination: process.env.NEXT_PUBLIC_API_URL
+          ? `${process.env.NEXT_PUBLIC_API_URL}/:path*`
+          : "http://127.0.0.1:8000/:path*",
       },
     ];
   },
